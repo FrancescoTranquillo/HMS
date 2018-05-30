a5992ea46ec5b0faa1c94c0c35c606a27dfcc0e9
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-05-30 19:03:28 +0200

    GIFSS
    removed technical, continued to add informative section:

--- 1)upload.md
+++ 1)upload.md
@@ -1,8 +1,9 @@
 # Upload your data
 
-To upload your data to Serena simply locate your file into your computer through the "browse" button and select it.
+To upload your data, simply locate the file through the "browse" button and select it:
 
 <img src="www/upload.png" align="center" />
 
+You can also upload your data simply using the **"Drag&Drop"** feature
 
-You can also upload your data simply using the "Drag&Drop" feature
+<img src="www/upload.gif">
--- app.R
+++ app.R
@@ -36,7 +36,6 @@ Sidebar<-dashboardSidebar(
     menuItem(tags$strong("Welcome"), tabName="Welcomepage", icon=icon("home"), selected= TRUE),
     menuItem(tags$b("Employee"), tabName= "Employee", icon=icon("user")),
     menuItem(tags$b("Specialized Practitioner"), tabName= "SP", icon = icon("user-md")),
-    menuItem(tags$b("Technical Administrator"), tabName="TA", icon=icon("info-circle")),
     menuItem(tags$b("Source code"), icon = icon("file-code-o"), href = "https://github.com/FrancescoTranquillo/Serena"),
     tags$img(src="logo2.png", height="auto",width="auto")
   )
@@ -56,11 +55,11 @@ body<-dashboardBody(
 
                             ),
                             tabPanel("1 Upload your data",
-                            includeMarkdown("2)visualize.md")
-                                    
+                            includeMarkdown("1)upload.md")
+
                             ),
                             tabPanel("2 Visualize your data",
-                                     p("As soon as your data will be uploaded, you will be able to visualize different temporal trends and plots regarding your parameter")
+                                     includeMarkdown("2)visualize.md")
                             ),
                             tabPanel("3 HEy use MS",
                                      includeMarkdown("README.md"))
@@ -211,41 +210,7 @@ body<-dashboardBody(
             )
     )
 
-,
-    #### TECHNICAL ADMIN #####
-    tabItem(tabName = "TA",
-    h2(
-      icon("gear"),
-      tags$b("Users data visualization")
-    ),
-    #####FIRST ROW#####
-    fluidRow(
 
-      box( title = strong("Upload your file here"),
-           solidHeader = TRUE, status = "success", background = "green",
-
-
-           fileInput("file3", label = NULL, accept = c("text/csv","text/comma-separated-values,text/plain",".csv"))
-
-
-
-      ),
-      box( title = strong("Help me"),
-           solidHeader = TRUE, status = "warning", collapsible = TRUE, collapsed = TRUE,
-
-           "Serena accepts only files with a", strong(".csv"),"extension. To upload a file, click on the Browse button on the left and select your file from your computer.",
-           br(),
-           br(),
-           "Too slow?",
-           br(),
-           "Serena supports Drag&Drop feature! Drag your file and drop it in the Browse button!",
-           br(),
-           br(),
-           "When the file will be uploaded, you can expand the next boxto visualize and interact with your data"
-      )
-    )
-
-  )
 )
 )
 
